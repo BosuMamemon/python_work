9177db3002623cdd2e8e49c128d99f03f2ba3c33
DESKTOP-0C7D4H8\it <makoid0104@gmail.com>
2025-02-24 16:42:15 +0900

    2025-02-24 16:41 학원에서 커밋

---
+++ python_work/mod1.py
@@ -0,0 +1,18 @@
+PI = 3.14159265357989
+
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+if __name__ == '__main__':
+    print(add(10, 20))
+    print(sub(10, 20))
---
+++ python_work/mod2.py
@@ -0,0 +1,5 @@
+import mod1 # 모듈에서도 다른 모듈을 불러올 수 있음
+
+def show():
+    print(mod1.PI)
+    print(mod1.add)
